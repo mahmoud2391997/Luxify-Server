33db5137003d916f3f177c03bc11e394e811c5bb
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-12-09 21:25:39 +0200

    deploying

--- api/server.js
+++ api/server.js
@@ -2,14 +2,13 @@ const express = require("express"); // Import Express
 const app = express(); // Create an Express app
 const PORT = 3000; // Define the port
 const mongoose = require("mongoose");
-const homeRouter = require("./routes/home.route");
+const homeRouter = require("../routes/home.route");
 require("dotenv").config();
 const dbUri = process.env.DB_CONNECTION_STRING; // Make sure this is set in your .env file
 app.use(express.json());
 
-// Define a simple route
-app.use("/api/home", homeRouter);
 // Start the server
+console.log(dbUri);
 
 mongoose
   .connect(dbUri, { dbName: "Luxify" })
