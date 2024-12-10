bf9192d90e91aa277dc5ff76455bc116ddc561a0
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-12-10 15:24:33 +0200

    still deploying

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express"); // Import Express
 const app = express(); // Create an Express app
-const PORT = 3000; // Define the port
+const PORT = 3001; // Define the port
 const mongoose = require("mongoose");
-const homeRouter = require("../routes/home.route");
+const homeRouter = require("./routes/home.route");
 require("dotenv").config();
 const dbUri = process.env.DB_CONNECTION_STRING; // Make sure this is set in your .env file
 app.use(express.json());
